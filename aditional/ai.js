c9e89643726db0bbb542322383a228c84f9cb5e9
vikas KH <108401249+VikasNayak1@users.noreply.github.com>
2023-12-01 15:14:15 +0530

    Update ai.js

--- aditional/ai.js
+++ aditional/ai.js
@@ -9,14 +9,13 @@ const API_KEY = "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";
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
-
     const chatLi = document.createElement("li");
     chatLi.classList.add("chat", `${className}`);
     let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
     chatLi.innerHTML = chatContent;
     chatLi.querySelector("p").textContent = message;
     return chatLi; 
-}
+}   
 
 const generateResponse = (chatElement) => {
     const API_URL = "https://api.openai.com/v1/chat/completions";
