1b14d17ae8332674823d21f5c2f0fd5a7fac1523
vikas KH <108401249+VikasNayak1@users.noreply.github.com>
2023-12-01 10:57:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const { latitude, longitude } = position.coords;
+      const apiKey = "3fc02434525f3efcf790a2908bb2322e";
+      const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+      try {
+        const response = await fetch(weatherApiUrl);
+        const data = await response.json();
+
+        // Convert temperature from Kelvin to Celsius
+        const temperatureCelsius = (data.main.temp - 273.15).toFixed(2);
+
+        // Update your weather data with location name
+        const weatherData = `Location: ${data.name}\nTemperature: ${temperatureCelsius} °C, Description: ${data.weather[0].description}`;
+        document.getElementById("weather-data").innerText = weatherData;
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
+    }, (error) => {
+      console.error("Error getting location:", error.message);
+    });
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+  }
+});
+
